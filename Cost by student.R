50440bccdd9597c39c63c843829e45b693f5efe8
dorianflagrant <46851450+dorianflagrant@users.noreply.github.com>
2019-01-24 11:56:03 +0100

    Update Cost by student.R

--- Cost by student.R
+++ Cost by student.R
@@ -2,14 +2,14 @@ Coststudent <- read.csv("Data/Cost-by-student.csv",
                          sep = ",",header = TRUE,
                          dec = ".")
 
-##Narrow down to tertiary education, all expenditure types, year 2015 [there was no data for 2016]
+#Narrow down to tertiary education, all expenditure types, year 2015 [there was no data for 2016]
 
 Coststudenttertiary <- Coststudent %>% filter(Education.level.and.programe.orientation == 
                                                 "Total tertiary education (ISCED2011 levels 5 to 8)",
                                               Type.of.expenditure == "All expenditure types",
                                               Year == 2015)
 
-##Differentiating ALL institutions / PUBLIC institutions / PRIVATE institutions
+#Differentiating ALL institutions / PUBLIC institutions / PRIVATE institutions
 
 CoststudenttertiaryPUB <- Coststudenttertiary %>% filter(Institution.type == "Public educational institutions")
 
@@ -17,23 +17,9 @@ CoststudenttertiaryALL <- Coststudenttertiary %>% filter(Institution.type == "Al
 
 CoststudenttertiaryPRIV <- Coststudenttertiary %>% filter(Institution.type == "All private educational institutions")
 
-##Graphs
+#Graphs
 
-CoststudenttertiaryPUB %>%
-  filter(Value>0) %>%
-  ggplot() +
-  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
-           position = position_stack(reverse = TRUE)) +  
-  coord_flip() +
-  theme()
-
-CoststudenttertiaryPRIV %>%
-  filter(Value>0) %>%
-  ggplot() +
-  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
-           position = position_stack(reverse = TRUE)) +  
-  coord_flip() +
-  theme()
+##All institutions
 
 CoststudenttertiaryALL %>%
   filter(Value>0) %>%
@@ -41,5 +27,34 @@ CoststudenttertiaryALL %>%
   geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
            position = position_stack(reverse = TRUE)) +  
   coord_flip() +
-  theme()
+  ggtitle("Cost / Tertiary education student (All)") +
+  xlab("OECD countries") +
+  ylab("% cost by student in tertiary education in total public expenditure") + 
+  labs(fill = "Cost ($)")
+
+##Public institutions
+
+CoststudenttertiaryPUB %>%
+  filter(Value>0) %>%
+  ggplot() +
+  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
+           position = position_stack(reverse = TRUE)) +  
+  coord_flip() +
+  ggtitle("Cost / Tertiary education student (Public institutions)") +
+  xlab("OECD countries") +
+  ylab("% cost by student in tertiary education in total public expenditure") + 
+  labs(fill = "Cost ($)")
+
+##Private institutions
+
+CoststudenttertiaryPRIV %>%
+  filter(Value>0) %>%
+  ggplot() +
+  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
+           position = position_stack(reverse = TRUE)) +  
+  coord_flip() +
+  ggtitle("Cost / Tertiary education student (Private institutions)") +
+  xlab("OECD countries") +
+  ylab("% cost by student in tertiary education in total public expenditure") + 
+  labs(fill = "Cost ($)")
 
