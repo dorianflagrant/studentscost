ef301c37d8c3827b3ca952cae387a442bd5570eb
dorianflagrant <46851450+dorianflagrant@users.noreply.github.com>
2019-01-24 14:13:14 +0100

    Add files via upload

--- Cost by student.R
+++ Cost by student.R
@@ -1,15 +1,17 @@
+### Cost by Tertiary education student in OECD countries ###
+
 Coststudent <- read.csv("Data/Cost-by-student.csv",
                          sep = ",",header = TRUE,
                          dec = ".")
 
-#Narrow down to tertiary education, all expenditure types, year 2015 [there was no data for 2016]
+## Narrow down to tertiary education, all expenditure types, year 2015 [there was no data for 2016]
 
 Coststudenttertiary <- Coststudent %>% filter(Education.level.and.programe.orientation == 
                                                 "Total tertiary education (ISCED2011 levels 5 to 8)",
                                               Type.of.expenditure == "All expenditure types",
                                               Year == 2015)
 
-#Differentiating ALL institutions / PUBLIC institutions / PRIVATE institutions
+## Differentiating ALL institutions / PUBLIC institutions / PRIVATE institutions
 
 CoststudenttertiaryPUB <- Coststudenttertiary %>% filter(Institution.type == "Public educational institutions")
 
@@ -17,44 +19,61 @@ CoststudenttertiaryALL <- Coststudenttertiary %>% filter(Institution.type == "Al
 
 CoststudenttertiaryPRIV <- Coststudenttertiary %>% filter(Institution.type == "All private educational institutions")
 
-#Graphs
+## Graphs - All 3 graphs are generated
 
-##All institutions
-
-CoststudenttertiaryALL %>%
-  filter(Value>0) %>%
-  ggplot() +
-  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
-           position = position_stack(reverse = TRUE)) +  
-  coord_flip() +
-  ggtitle("Cost / Tertiary education student (All)") +
-  xlab("OECD countries") +
-  ylab("% cost by student in tertiary education in total public expenditure") + 
-  labs(fill = "Cost ($)",caption = "Source: OECD")
-
-##Public institutions
+  # Public institutions
 
 CoststudenttertiaryPUB %>%
   filter(Value>0) %>%
+  #highlighting France in the bar charts
+  mutate (highlight_flag = ifelse(COUNTRY == 'FRA', T, F)) %>% 
   ggplot() +
-  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
+  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = highlight_flag), 
+           stat = "identity", 
            position = position_stack(reverse = TRUE)) +  
+  scale_fill_manual(values = c('#00aaff', 'red')) +
   coord_flip() +
-  ggtitle("Cost / Tertiary education student (Public institutions)") +
+  ggtitle("Cost / Tertiary education student (Public)") +
   xlab("OECD countries") +
   ylab("% cost by student in tertiary education in total public expenditure") + 
-  labs(fill = "Cost ($)",caption = "Source: OECD")
+  labs(fill = "Cost ($)",caption = "Source: OECD") +
+  theme(legend.position = 'none', 
+        plot.title = element_text(size = 18, face = 'bold'))
 
-##Private institutions
+  # Private institutions
+  
+  CoststudenttertiaryPRIV %>%
+    filter(Value>0) %>%
+    #highlighting France in the bar charts
+    mutate (highlight_flag = ifelse(COUNTRY == 'FRA', T, F)) %>% 
+    ggplot() +
+    geom_bar(aes(x = reorder(Country, Value), y = Value, fill = highlight_flag), 
+             stat = "identity", 
+             position = position_stack(reverse = TRUE)) +  
+    scale_fill_manual(values = c('#00aaff', 'red')) +
+    coord_flip() +
+    ggtitle("Cost / Tertiary education student (Private)") +
+    xlab("OECD countries") +
+    ylab("% cost by student in tertiary education in total public expenditure") + 
+    labs(fill = "Cost ($)",caption = "Source: OECD") +
+    theme(legend.position = 'none', 
+          plot.title = element_text(size = 18, face = 'bold'))
 
-CoststudenttertiaryPRIV %>%
-  filter(Value>0) %>%
-  ggplot() +
-  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
-           position = position_stack(reverse = TRUE)) +  
-  coord_flip() +
-  ggtitle("Cost / Tertiary education student (Private institutions)") +
-  xlab("OECD countries") +
-  ylab("% cost by student in tertiary education in total public expenditure") + 
-  labs(fill = "Cost ($)",caption = "Source: OECD")
-
+  # All institutions
+  
+  CoststudenttertiaryALL %>%
+    filter(Value>0) %>%
+    #highlighting France in the bar charts
+    mutate (highlight_flag = ifelse(COUNTRY == 'FRA', T, F)) %>% 
+    ggplot() +
+    geom_bar(aes(x = reorder(Country, Value), y = Value, fill = highlight_flag), 
+             stat = "identity", 
+             position = position_stack(reverse = TRUE)) +  
+    scale_fill_manual(values = c('#00aaff', 'red')) +
+    coord_flip() +
+    ggtitle("Cost / Tertiary education student (All)") +
+    xlab("OECD countries") +
+    ylab("% cost by student in tertiary education in total public expenditure") + 
+    labs(fill = "Cost ($)",caption = "Source: OECD") +
+    theme(legend.position = 'none', 
+          plot.title = element_text(size = 18, face = 'bold'))
--- Tertiary education expenditure.R
+++ Tertiary education expenditure.R
@@ -1,21 +1,20 @@
+### Tertiary education expenditure in overall expenditure ###
+
 install.packages("tidyverse")
 library("tidyverse")
 
-##Public expenditure on education as percentage of total government expenditure
+## Public expenditure on education as percentage of total government expenditure
 
 expenditures <- read.csv("Data/EAG_FIN_RATIO_CATEGORY_23012019145211397.csv",
                          sep = ",",header = TRUE,
                          dec = ".")
 
-View(expenditures)
-names(expenditures)
-
-##Narrow it down to tertiary education expenditures
+## Narrow it down to tertiary education expenditures
 
 tertiaryexp <- filter(expenditures, 
                       Education.level.and.programe.orientation == "Total tertiary education (ISCED2011 levels 5 to 8)")
 
-##Narrow it down to tertiary education expenditures in 2015
+## Narrow it down to tertiary education expenditures in 2015
 
 tertiaryexp <- filter(expenditures, 
 Education.level.and.programe.orientation == "Total tertiary education (ISCED2011 levels 5 to 8)",
@@ -23,20 +22,25 @@ Year == 2015, Institution.type == "All sectors")
 
 names(tertiaryexp)
 
-##Keep only the country and the total amount dedicated to tertiary education expenditures
+## Keep only the country and the total amount dedicated to tertiary education expenditures
 
 tertiaryexpsimple <- tertiaryexp[c(2,15)]
 
-##Graphs
+## Graph
 
 tertiaryexpsimple %>%
   filter(Value>0) %>%
+  #highlighting France in the bar charts
+  mutate (highlight_flag = ifelse(Country == 'France', T, F)) %>% 
   ggplot() +
-  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = Value), stat = "identity", 
+  geom_bar(aes(x = reorder(Country, Value), y = Value, fill = highlight_flag), 
+           stat = "identity", 
            position = position_stack(reverse = TRUE)) +  
+  scale_fill_manual(values = c('#00aaff', 'red')) +
   coord_flip() +
   ggtitle("Tertiary education expenditure in OECD countries") +
   xlab("OECD countries") +
   ylab("% tertiary education expenditure / Total public expenditure") + 
-  labs(fill = "%",caption = "Source: OECD")
-
+  labs(fill = "%",caption = "Source: OECD") +
+  theme(legend.position = 'none', 
+        plot.title = element_text(size = 15, face = 'bold'))
